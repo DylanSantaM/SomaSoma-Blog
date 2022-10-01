1d4fd3949f3c7208d8c74333aba41368ae8cea2a
flaniboy <flanalwaysgood@gmail.com>
2022-10-01 13:28:05 +0300

    Instantiate Flask Packages

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,15 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
+
+db = SQLAlchemy(app)
+migrate = Migrate(db, app)
+bootstrap = Bootstrap(app)
+login = LoginManager(app)
+login.login_view = "login"
 
 from app import routes, models, errors
