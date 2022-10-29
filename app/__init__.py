22787edc75c6e5c87d7de3ad4530c01677c8f568
flaniboy <flanalwaysgood@gmail.com>
2022-10-29 11:54:00 +0300

    create application instance

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,12 @@ from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from config import Config
 
 app = Flask(__name__)
-
+app.config.from_object(Config)
 db = SQLAlchemy(app)
-migrate = Migrate(db, app)
+migrate = Migrate(app, db)
 bootstrap = Bootstrap(app)
 login = LoginManager(app)
 login.login_view = "login"
